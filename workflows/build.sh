da65001278bd34b24def3284844b427216c1f401
nuuuwan <nuuuwan@gmail.com>
2025-11-13 10:51:17 +0530

    refactor: update commit message format in build script to include 'local'

--- workflows/build.sh
+++ workflows/build.sh
@@ -1,6 +1,6 @@
 python3 workflows/build.py
 git add README.md
 git add data
-git commit -m "[build.py] $(date '+%Y-%m-%d-%H%M')"
+git commit -m "[build.py] local $(date '+%Y-%m-%d-%H%M')"
 git push origin main
 open https://github.com/nuuuwan/lk_news_digest/blob/main/README.md
