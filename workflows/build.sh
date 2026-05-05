90aad2c28a2aed3572204def58b3bbfda059782f
nuuuwan <nuuuwan@gmail.com>
2026-05-05 09:36:28 +0530

    Remove build script for automated deployment

--- workflows/build.sh
+++
@@ -1,7 +0,0 @@
-python3 workflows/build.py
-git add README.md
-git add rss.xml
-git add data
-git commit -m "[build.py] local $(date '+%Y-%m-%d-%H%M')"
-git push origin main
-open https://github.com/nuuuwan/lk_news_digest/blob/main/README.md
